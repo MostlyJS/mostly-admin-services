b262576d26d2e4722b3c3c75d16293aa300593b7
forafish <fora.luo@gmail.com>
2017-06-19 18:13:58 +0800

    Auth config

--- src/authentication.js
+++ src/authentication.js
@@ -2,26 +2,26 @@ import auth from 'feathers-authentication';
 import jwt from 'feathers-authentication-jwt';
 import local from 'feathers-authentication-local';
 import { hooks } from 'mostly-feathers-mongoose';
-import { config } from 'common';
 
-export default function(app) {
-  
-  app.set('auth', config.auth);
-  app.configure(auth(config.auth));
-  app.configure(jwt());
-  app.configure(local());
+export default function(options) {
+  return function(app) {
+    app.set('auth', options);
+    app.configure(auth(options));
+    app.configure(jwt());
+    app.configure(local());
 
-  app.service('authentication').hooks({
-    before: {
-      create: [
-        auth.hooks.authenticate('jwt', 'local')
-      ],
-      remove: [
-        auth.hooks.authenticate('jwt')
-      ]
-    },
-    after: {
-      all: [ hooks.responder() ]
-    }
-  });
-}
+    app.service('authentication').hooks({
+      before: {
+        create: [
+          auth.hooks.authenticate('jwt', 'local')
+        ],
+        remove: [
+          auth.hooks.authenticate('jwt')
+        ]
+      },
+      after: {
+        all: [ hooks.responder() ]
+      }
+    });
+  }
+};
