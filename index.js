330d862cba48c30b25af3a4c0bfa08caa19be6ae
forafish <fora.luo@gmail.com>
2017-07-01 20:05:11 +0800

    Rename filename

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports.authentication = require('./lib/authentication');
-module.exports.actionsStats = require('./lib/actions-stats');
-module.exports.processStats = require('./lib/process-stats');
+module.exports.actionsStats = require('./lib/actions_stats');
+module.exports.processStats = require('./lib/process_stats');
